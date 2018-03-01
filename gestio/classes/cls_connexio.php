6af395ffd99c617c230ad1cac4bc6140b49fb439
root <benjaminzaragoza@iesebre.com>
2018-03-01 18:33:47 +0100

    segon

--- gestio/classes/cls_connexio.php
+++ gestio/classes/cls_connexio.php
@@ -1,8 +1,74 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class connexio{
+    var $host;
+    var $db;
+    var $user;
+    var $password;
+    var $conn;
+    
+    function connexio($ruta="../../"){
+        $this->host="localhost";
+        $this->db="pt3";
+        $this->user="alumne";
+        $this->passwd="alumne";
+    }
+    function DB_Open(){
+        $this->conn = mysqli_connect($this->host, $this->user, $this->password);
+        if($this->conn){
+            if (! mysqli_select_db($this->conn, $this->db)){
+                
+            }else{
+                $status=0;
+            }
+        } else {
+           $status = mysqli_error();
+        }
+        return ($status);
+    }
+    
+    function DB_Close(){
+        if (mysqli_close($this->conn)){
+            $status=1;
+  
+        }else{
+            $status=0;
+        }
+        return($status);
+    }
+    
+    function DB_Select($strSelect){
+        $this->DB_Open();
+        $result = mysqli_query($this->conn,$strSelect);
+        if($result){
+            if (mysqli_num_rows($result)> 0) {
+                return ($result);
+            }else{
+                return(0);
+            }
+        }else{
+            $result= mysqli_error();
+        }
+        $this->DB_Close();
+    }
+    
+    function  DB_Execute($strSelect){
+        $this->DB_Open();
+        $result = mysqli_query($this->conn,$strSelect);
+        $this->DB_Close();
+    }
+    
+    function DB_Fetch($resultat){
+        if($resultat){
+             if (mysqli_num_rows($resultat)> 0) {
+                return (mysqli_fetch_array($resultat));
+            }else{
+                return false ;
+            }
+        } else {
+            return false ;    
+        }
+    }
+}
+?>
 
--- gestio/classes/cls_general.php
+++ gestio/classes/cls_general.php
@@ -1,8 +1,25 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class general extends connexio{
+    var $conn;
+    function autor($ruta="../../"){
+        parent::connexio($ruta);
+    }
+
+    function llistat_autors(){
+        $sql="SELECT AUTH_IDAUTOR FROM AUTORS";
+        $rs= $this->DB_Select($sql);
+        $i=1;
+        
+        while ($rs_f=$this->DB_Fetch($rs)){
+            $aut=new autor();
+            $aut->inicialitza($rs_f['AUT_IDAUTOR']);
+            $items[$i]= serialize($aut);
+            $i=$i++;
+        }
+        return $items;
+    }
+    
+    }
+?>
 
